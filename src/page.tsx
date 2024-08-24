571e4e461225fab84514638e2e423351d867d1f3
MARK MOODY <enigmaitech1@gmail.com>
2024-08-24 13:24:20 -0500

    Update page.tsx

--- src/page.tsx
+++ src/page.tsx
@@ -1,12 +1,7 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import HeroSection from './HeroSection'
-import FormAndWhyChooseUs from './FormAndWhyChooseUs'
-import AboutAntares from './AboutAntares'
-import DiscountSlider from './DiscountSlider'
-import StatisticsSection from './StatisticsSection'
-import LuxuryGallerySection from './LuxuryGallerySection'
-import BeachPartiesSection from './BeachPartiesSection'
+
 
 export default function Home() {
   return (
@@ -15,12 +10,6 @@ export default function Home() {
       <Link href="/booking">
          <Button>Book Now</Button>
       </Link>
-      <FormAndWhyChooseUs />
-      <AboutAntares />
-      <DiscountSlider />
-      <StatisticsSection />
-      <LuxuryGallerySection />
-      <BeachPartiesSection />
     </main>
   );
 }
